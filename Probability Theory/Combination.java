697a04bf73caee75859015545da88c749bc3a0fe
Yeri Lee <64480162+leeyr0412@users.noreply.github.com>
2023-03-04 16:36:37 +0900

    Create Combination.java

---
+++ Probability Theory/Combination.java
@@ -0,0 +1,23 @@
+public class Combination {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        combination(arr, 0, r,0);
+    }
+
+    static int n = 5, r = 3;
+    static int[] numbers = new int[r];
+    static void combination(int[] arr, int start, int r, int cnt) {
+        if (cnt == r) {
+            for(int i = 0; i < r; i++){
+                System.out.print(numbers[i]+" ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = start; i < n; i++) {
+            numbers[cnt] = arr[i];
+            combination(arr, i + 1, r,cnt+1);
+        }
+    }
+}
